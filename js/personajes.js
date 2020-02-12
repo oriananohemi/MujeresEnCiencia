39ab0f31285472d59f93f86e8bde699d40fe72b9
Oriana Reina <oriana.nohemi22@gmail.com>
2020-02-12 11:19:35 -0500

    Agregar interaccion con el usuario. El usuario puede ingresar el nombre del personaje que aparece en cada foto

---
+++ js/personajes.js
@@ -0,0 +1,67 @@
+function marie() {
+  var nameP = prompt('Ingresa el nombre del personaje').toLowerCase()
+
+  // nameP = nameP.toLowerCase()
+
+  if (nameP == 'marie curie' || nameP == 'curie') {
+    swal({
+      title: 'Correcto',
+      text: 'Pionera en el campo de la radioactividad, fue la primera mujer en recibir el premio Nobel de Fisica y la primera persona en recibir dos Nobel en distintas especialidades',
+      type: 'success',
+      button: 'Oki :)'
+    });
+  } else {
+    swal('Oh no, intentalo de nuevo');
+  }
+}
+
+function katie() {
+  var nameP = prompt('Ingresa el nombre del personaje').toLowerCase()
+
+  // nameP = nameP.toLowerCase()
+
+  if (nameP == 'katie bouman' || nameP == 'bouman') {
+    swal({
+      title: 'Correcto',
+      text: 'Cientifica de la computacion que investiga nuevos sistemas de formacion de imagenes por ordenador',
+      type: 'success',
+      button: 'Oki :)'
+    });
+  } else {
+    swal('Oh no, intentalo de nuevo')
+  }
+}
+
+function maria() {
+  var nameP = prompt('Ingresa el nombre del personaje').toLowerCase()
+
+  // nameP = nameP.toLowerCase()
+
+  if (nameP == 'maria goeppert-mayer ' || nameP == 'goeppert-mayer') {
+    swal({
+      title: 'Correcto',
+      text: 'fue la segunda mujer galardonada con el premio nobel',
+      type: 'success',
+      button: 'Oki :)'
+    });
+  } else {
+    swal('Oh no, intentalo de nuevo')
+  }
+}
+
+function hamilton() {
+  var nameP = prompt('Ingresa el nombre del personaje').toLowerCase()
+
+  // nameP = nameP.toLowerCase()
+
+  if (nameP == 'margaret hamilton' || nameP == 'hamilton') {
+    swal({
+      title: 'Correcto',
+      text: 'La compañía se desarrolló alrededor del lenguaje universal de sistemas basada en su paradigma de "desarrollo antes del hecho"',
+      type: 'success',
+      button: 'Oki :)'
+    });
+  } else {
+    swal('Oh no, intentalo de nuevo')
+  }
+}
